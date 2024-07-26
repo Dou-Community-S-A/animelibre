1e326742be2694e37037f3f43507aeba953cf5e4
matiasdante <mdantesarco@uade.edu.ar>
2024-07-26 19:34:24 -0300

    Co-authored-by: lostdou <Lostdou@users.noreply.github.com> Co-authored-by: Shackmerferu <Shakmerferu@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,29 @@
+from animeflv import AnimeFLV, EpisodeInfo, AnimeInfo
+
+with AnimeFLV() as api:
+    try:
+        anime_name = input("Anime: ")
+        anime_list = api.search(anime_name)
+        for i, anime in enumerate(anime_list):
+            print(f"{i} - {anime.title}")
+
+        select = int(input("Opción: "))
+        selected_anime = anime_list[select]
+        info = api.get_anime_info(selected_anime.id)
+        info.episodes.reverse()
+
+        for j, episode in enumerate(info.episodes):
+            print(f"{j} - {episode.id}")
+
+        ep = int(input("Opción de episodio: "))
+        serie = selected_anime.id
+        capitulo = info.episodes[ep].id
+        links = api.get_links(serie, capitulo)
+
+        for link in links:
+            print(f"{link.server} - {link.url}")
+
+    except Exception as e:
+        print(e)
+
+    
