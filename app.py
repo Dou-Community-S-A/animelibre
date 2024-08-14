de7fa38eb63a2477e86f8393d34168f65122b335
lostdou <facundobott@gmail.com>
2024-08-14 12:33:16 -0300

    New menu and code re-ordered

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from tkinter.font import Font
 from ttkbootstrap import Style
 import os
 from scripts.anime_scrapper import AnimeFLV
-import os
 import threading
 from zipfile import ZipFile
 from requests import get
@@ -35,114 +34,166 @@ import mpv
 locale.setlocale(locale.LC_NUMERIC, 'C')
 
 class AnimeApp:
-    def __init__(self, root):
+    def __init__(self, root): # Crear ventana principal
         self.root = root
-        self.root.title("Anime Libre v1.0")
+        self.root.title("Anime Libre v1.1")
         self.root.resizable(0, 0)
         self.root.geometry('1024x800')
         self.root.iconbitmap(r'assets\mainicon.ico')
         self.api = AnimeFLV()
 
-        # Aplicar fondo
+        self.widgets = []  # Lista para almacenar widgets
+        self.set_background() # aplica el fondo
+        self.mainmenu() # llama al menu principal
+
+    def set_background(self): # Aplicar fondos de pantalla
         bg_image = Image.open(r"assets/background/background.jpg")
-        bg_image = bg_image.filter(ImageFilter.GaussianBlur(radius=6))
+        bg_image = bg_image.resize((1024,800), Image.Resampling.LANCZOS)
+        bg_image = bg_image.filter(ImageFilter.GaussianBlur(radius=4))
         enhancer = ImageEnhance.Brightness(bg_image)
         bg_image_darker = enhancer.enhance(0.5)
         self.bg_photo = ImageTk.PhotoImage(bg_image_darker)
 
-        canvas = tk.Canvas(self.root, width=1024, height=768)
+        canvas = tk.Canvas(self.root, width=1024, height=800)
         canvas.pack(fill="both", expand=True)
         canvas.create_image(0, 0, image=self.bg_photo, anchor="nw")
 
         style = Style(theme="darkly")  # Aplicar un tema oscuro
 
+    def clear_widgets(self): # Limpiar los widgets
+        for widget in self.widgets:
+            widget.destroy()
+        self.widgets.clear()
 
-        ## ============== header_frame ======================
+    def global_widgets(self): # Widgets utilizados en todos los menús
         header_frame = ttk.Frame(self.root)
         header_frame.place(x=10, y=10)
+        self.widgets.append(header_frame)
 
         title_label = ttk.Label(header_frame, text="Anime Libre", font=("Osaka-Sans Serif", 20))
         title_label.pack(anchor="nw")
+        self.widgets.append(title_label)
 
         subtitle_label = ttk.Label(header_frame, text="By Dou Community", foreground="#00a3ff", font=("Helvetica", 10), cursor="hand2")
         subtitle_label.pack(anchor="nw")
         subtitle_label.bind("<Button>", lambda event: callback("https://github.com/Dou-Community-S-A"))
+        self.widgets.append(subtitle_label)
+
+        mainmenu_button=ttk.Button(self.root, text="Menu Principal", cursor="hand2", command=self.mainmenu)
+        mainmenu_button.place(x=800, y=20)
+        self.widgets.append(mainmenu_button)
+
+        optionsmenu_button=ttk.Button(self.root, text="Opciones", cursor="hand2", command=self.optionsmenu)
+        optionsmenu_button.place(x=910, y=20)
+        self.widgets.append(optionsmenu_button)
+    
+    def mainmenu(self): # Menu principal (Busqueda de anime)
+        self.clear_widgets()
+        self.global_widgets()
 
         ## ============== search_frame ======================
         search_frame = ttk.Frame(self.root)
         search_frame.place(x=200, y=20)
+        self.widgets.append(search_frame)
 
         search_label = ttk.Label(search_frame, text="Buscar Anime:")
         search_label.pack(side=tk.LEFT)
+        self.widgets.append(search_label)
 
         self.search_entry = ttk.Entry(search_frame, width=50)
         self.search_entry.pack(side=tk.LEFT, padx=10)
+        self.widgets.append(self.search_entry)
 
-        search_button = ttk.Button(search_frame, text="Buscar", command=self.search_anime)
+        search_button = ttk.Button(search_frame, text="Buscar", cursor="hand2", command=self.search_anime)
         search_button.pack(side=tk.LEFT)
+        self.widgets.append(search_button)
 
         ## ============== main_frame ======================
         main_frame = ttk.Frame(self.root)
         main_frame.place(x=10, y=80, width=1004, height=320)
+        self.widgets.append(main_frame)
 
         # Lista de resultados de búsqueda y imagen de portada
         results_frame = ttk.Frame(main_frame)
         results_frame.place(x=0, y=0, width=700, height=350)
+        self.widgets.append(results_frame)
 
         self.results_listbox = tk.Listbox(results_frame, height=10, bg='black', fg='white')
         self.results_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.results_listbox.bind('<<ListboxSelect>>', self.load_anime_info)
+        self.widgets.append(self.results_listbox)
 
         results_scrollbar = ttk.Scrollbar(results_frame, orient=tk.VERTICAL, command=self.results_listbox.yview)
         results_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.results_listbox.config(yscrollcommand=results_scrollbar.set)
+        self.widgets.append(results_scrollbar)
 
         # Frame para la imagen de portada
         cover_frame = ttk.Frame(main_frame)
         cover_frame.place(x=710, y=0, width=284, height=350)
+        self.widgets.append(cover_frame)
 
         self.cover_label = ttk.Label(cover_frame)
         self.cover_label.pack(side=tk.TOP, pady=(10, 10))
+        self.widgets.append(self.cover_label)
 
         ## ============== info_frame ======================
         info_frame = ttk.Frame(self.root)
         info_frame.place(x=10, y=410, width=1004, height=200)
+        self.widgets.append(info_frame)
 
         self.info_text = tk.Text(info_frame, height=10, wrap=tk.WORD)
         self.info_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.info_text.config(state=tk.DISABLED)
+        self.widgets.append(self.info_text)
 
         self.episodes_listbox = tk.Listbox(info_frame, height=10)
         self.episodes_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.episodes_listbox.bind('<<ListboxSelect>>', self.load_episode_links)
+        self.widgets.append(self.episodes_listbox)
 
         episodes_scrollbar = ttk.Scrollbar(info_frame, orient=tk.VERTICAL, command=self.episodes_listbox.yview)
         episodes_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.episodes_listbox.config(yscrollcommand=episodes_scrollbar.set)
+        self.widgets.append(episodes_scrollbar)
 
         ## ============== links_frame ======================
         links_frame = ttk.Frame(self.root)
         links_frame.place(x=10, y=620, width=1004, height=130)
+        self.widgets.append(links_frame)
 
         self.links_listbox = tk.Listbox(links_frame, height=10)
         self.links_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.links_listbox.bind('<<ListboxSelect>>', self.select_link)
+        self.widgets.append(self.links_listbox)
 
         links_scrollbar = ttk.Scrollbar(links_frame, orient=tk.VERTICAL, command=self.links_listbox.yview)
         links_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.links_listbox.config(yscrollcommand=links_scrollbar.set)
+        self.widgets.append(links_scrollbar)
 
         ## ============== play_button_frame ======================
         play_button_frame = ttk.Frame(self.root)
         play_button_frame.place(x=10, y=760, width=1004, height=30)
+        self.widgets.append(play_button_frame)
 
         self.progress_bar = ttk.Progressbar(play_button_frame, mode='indeterminate')
         self.progress_bar.pack(side=tk.LEFT, padx=10, fill=tk.X, expand=True)
+        self.widgets.append(self.progress_bar)
 
-        self.play_button = ttk.Button(play_button_frame, text="Play", command=self.play_selected_link)
+        self.play_button = ttk.Button(play_button_frame, text="Play", cursor="hand2", command=self.play_selected_link)
         self.play_button.pack(side=tk.RIGHT)
+        self.widgets.append(self.play_button)
         self.selected_link = None
 
+    def optionsmenu(self): # Menu de configuraciones (Selector de reproductor)
+        self.clear_widgets()
+        self.global_widgets()
+
+        options_label = ttk.Label(self.root, text="Opciones", font=("Helvetica", 16))
+        options_label.place(x=200, y=80)
+        self.widgets.append(options_label)
+        
     def search_anime(self):
         query = self.search_entry.get().strip()
         if query:
@@ -159,8 +210,8 @@ class AnimeApp:
             index = selection[0]
             anime_id = self.results_listbox.get(index)
             self.api.anime_info(anime_id)
-            self.info_text.delete(1.0, tk.END)
             self.info_text.config(state=tk.NORMAL)
+            self.info_text.delete(1.0, tk.END)
             self.info_text.insert(tk.END, f"Título: {self.api.anime_title()}\n")
             self.info_text.insert(tk.END, f"Estado: {self.api.anime_status()}\n")
             self.info_text.insert(tk.END, f"Sinopsis: {self.api.anime_summary()}\n")
